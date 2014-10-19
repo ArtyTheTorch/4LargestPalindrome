5d2fdea1def26b74a4e833f2170debaff717105d
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:28:18 -0700

    Finds The Largest Palindrome within a string
    Implemented recursive method to find check for palindromes.
    UI interface

---
+++ 4LargestPalindrome/FindLargestPalindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4LargestPalindrome
+{
+    class FindLargestPalindrome
+    {
+        static string str;
+        static string results;
+        IsPalindrome pal;
+
+        public FindLargestPalindrome(string p)
+        {
+            // TODO: Complete member initialization
+            str = p;
+            results = "";
+            pal = new IsPalindrome();
+        }
+
+        public string getResults()
+        {
+            string temp = str;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                for (int j = 2; j <= str.Length- i; j++)
+                {
+                    temp = str.Substring(i,j);
+                    if (pal.isPalindrome(temp))
+                    {
+                        if (temp.Length > results.Length)
+                        {
+                            results = temp;
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ 4LargestPalindrome/IsPalindrome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4LargestPalindrome
+{
+    class IsPalindrome
+    {
+        // Recursive method use to get the reverse of a string
+        public string getReverse(string str)
+        {
+            if (str.Length < 2)
+            {
+                return str;
+            }
+            else
+            {
+                return getReverse(str.Substring(1)) + str[0];
+            }
+        }
+        //recursively checks to see if a string is a plaindrome or not
+        public bool isPalindrome(string str)
+        {
+            return str == this.getReverse(str);
+        }
+    }
+}
